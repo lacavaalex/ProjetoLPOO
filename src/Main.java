cf3a9a5d686d63f41adae8c33c8fe467ed7a6342
lacavaalex <alexlacavajr@gmail.com>
2025-04-03 21:45:54 -0300

    Janela criada a partir da Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.JFrame; // Importei a classe JFrame, que me permite criar uma janela
+import java.awt.Color;
+
 public class Main {
+
  public static void main(String[] args) {
+
+  //Abertura e título da janela
+  JFrame janela = new JFrame();
+  janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  janela.getContentPane().setBackground(Color.black);
+  janela.setTitle("O MUNDO FUNESTO");
+
+  // Tamanho e localização da janela na tela (central)
+  janela.pack();
+  janela.setLocationRelativeTo(null);
+  janela.setVisible(true);
+  janela.setResizable(false);
+  janela.setSize(900, 700);
+
 
  }
 }

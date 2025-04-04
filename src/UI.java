acb2b20943c2858a0618c23aeacf3a71b3faf77f
lacavaalex <alexlacavajr@gmail.com>
2025-04-03 23:19:58 -0300

    Implementação de uma UI
    Implementação da classe de interface do usuário, assim como a adição do método paintComponent (Painel) e da definição dos tile sizes (sujeito a mudanças)

--- src/UI.java
+++ src/UI.java
@@ -1,3 +1,91 @@
+import java.awt.*;
+
 public class UI {
+
+    Painel painel;
+    Graphics2D g2;
+    Font pixelsans_30, pixelsans_60B;
+
+    private int telaInicialState = 0;
+
+    public UI(Painel painel) {
+        this.painel = painel;
+
+        pixelsans_30 = new Font("Pixel Sans Serif", Font.PLAIN, 30);
+        pixelsans_60B = new Font("Pixel Sans Serif", Font.BOLD, 60);
+    }
+
+    public void mostrar(Graphics2D g2) {
+        this.g2 = g2;
+
+        g2.setFont(pixelsans_30);
+        g2.setColor(Color.white);
+
+        //Title state
+        int gameState = painel.getGameState();
+        int titleState = painel.getTitleState();
+        if (gameState == titleState) {
+            mostrarTelaInicial();
+        }
+    }
+
+    public void mostrarTelaInicial() {
+
+        int tileSize = painel.getTileSize();
+        int larguraTela = painel.getLargura();
+        int alturaTela = painel.getAltura();
+
+
+        if (getTelaInicialState() == 0) {
+
+            // Título
+            g2.setColor(new Color(20, 0, 10));
+            g2.fillRect(0, 0, larguraTela, alturaTela);
+
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 35F));
+            String texto = "O MUNDO FUNESTO.";
+            int x = coordenadaXParaTextoCentralizado(texto);
+            int y = tileSize * 5;
+
+            g2.setColor(Color.white);
+            g2.drawString(texto, x, y);
+
+            // MENU (AINDA SEM COMANDOS)
+            g2.setFont(g2.getFont().deriveFont(Font.BOLD, 15F));
+
+            texto = "NOVO JOGO";
+            x = coordenadaXParaTextoCentralizado(texto);
+            y += tileSize * 2;
+            g2.drawString(texto, x, y);
+
+            texto = "CARREGAR JOGO";
+            x = coordenadaXParaTextoCentralizado(texto);
+            y += tileSize;
+            g2.drawString(texto, x, y);
+
+            texto = "SAIR";
+            x = coordenadaXParaTextoCentralizado(texto);
+            y += tileSize;
+            g2.drawString(texto, x, y);
+        }
+    }
+
+    public int coordenadaXParaTextoCentralizado(String texto) {
+
+        int larguraTela = painel.getLargura();
+
+        int comprimento = (int) g2.getFontMetrics().getStringBounds(texto, g2).getWidth();
+        int x = larguraTela / 2 - comprimento / 2;
+        return x;
+
+    }
+
+// Getters e setters
+    public int getTelaInicialState() {
+        return telaInicialState;
+    }
+    public void setTelaInicialState(int telaInicialState) {
+        this.telaInicialState = telaInicialState;
+    }
 
 }

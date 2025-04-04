29fc53bd151b32872d87210193edb9a4ea076c28
lacavaalex <alexlacavajr@gmail.com>
2025-04-04 01:53:36 -0300

    Controles e Nova Tela
    Criação da classe Controles, método de seleção de personagem com tela própria, e criação do placeholder para o playstate.

---
+++ src/Controles.java
@@ -0,0 +1,104 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Controles implements KeyListener {
+
+    Painel painel;
+
+    public Controles(Painel painel) {
+        this.painel = painel;
+    }
+
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        int gameState = painel.getGameState();
+        int titleState = painel.getTitleState();
+        int playState = painel.getPlayState();
+
+        UI  ui = painel.getUi();
+        int telaInicialState = ui.getTelaInicialState();
+        int numComando = ui.getNumComando();
+
+    //Tela inicial
+        if (gameState == titleState) {
+
+            if (telaInicialState == 0) {
+
+                if (code == KeyEvent.VK_W) {
+                    numComando--;
+                    if (numComando < 0) {
+                        numComando = 2;
+                    }
+                }
+                if (code == KeyEvent.VK_S) {
+                    numComando++;
+                    if (numComando > 2) {
+                        numComando = 0;
+                    }
+                }
+                if (code == KeyEvent.VK_ENTER) {
+                    if (numComando == 0) {
+                        telaInicialState = 1;
+                    }
+                    if (numComando == 1) {
+
+                    }
+                    if (numComando == 2) {
+                        System.exit(0);
+                    }
+                }
+
+            } else if (telaInicialState == 1) {
+
+                if (code == KeyEvent.VK_W) {
+                    numComando--;
+                    if (numComando < 0) {
+                        numComando = 4;
+                    }
+                }
+                if (code == KeyEvent.VK_S) {
+                   numComando++;
+                    if (numComando > 4) {
+                        numComando = 0;
+                    }
+                }
+                if (code == KeyEvent.VK_ENTER) {
+                    if (numComando == 0) {
+                        System.out.println("A Guerreira");
+                        painel.setGameState(playState);
+                    }
+                    if (numComando == 1) {
+                        System.out.println("O Sobrevivente");
+                        painel.setGameState(playState);
+                    }
+                    if (numComando == 2) {
+                        System.out.println("O Médico");
+                        painel.setGameState(playState);
+                    }
+                    if (numComando == 3) {
+                        System.out.println("A Fora da Lei");
+                        painel.setGameState(playState);
+                    }
+                    if (numComando == 4) {
+                        telaInicialState = 0;
+                    }
+                }
+            }
+        }
+
+    // Jogar a atualização dos valores dessas duas variáveis à sua classe natal
+        ui.setNumComando(numComando);
+        ui.setTelaInicialState(telaInicialState);
+    // Atualização gráfica
+        painel.repaint();
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+}

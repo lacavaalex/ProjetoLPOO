37ef0e2cc50d96ece96255cbbc2a5fcb0f23f3b2
lacavaalex <alexlacavajr@gmail.com>
2025-04-03 21:45:54 -0300

    Janela criada a partir da Main

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,2 +1,9 @@
 public class Jogador {
+
+    // inventário
+    //Rastreador, mecanico, sobrevivent
+    private String nome, localizacao;
+    private int vida, fome, sede, energia;
+    private boolean sanidade;
+
 }

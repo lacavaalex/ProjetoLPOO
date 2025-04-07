711a4e025c1638dbd8f31a42c780788408f4d978
lacavaalex <alexlacavajr@gmail.com>
2025-04-06 23:57:03 -0300

    Evento de Criatura associado à classe Evento
    Previamente, ele estava sendo criado diretamente na UI. Agora, criei uma classe, método, e utilizei da biblioteca Random para, futuramente, implementar probabilidade a todos os eventos aleatórios.

--- src/Entidade/Criatura.java
+++ src/Entidade/Criatura.java
@@ -1,4 +1,45 @@
 package Entidade;
 
+import Main.*;
+
 public class Criatura {
+
+    private String nome;
+    private int vida = 0;
+    private int ataque = 0;
+
+    public Criatura() {
+
+    }
+
+
+    public void setViboraRubro() {
+        setNomeCriatura("Víbora-Rubro");
+        setVidaCriatura(3);
+        setAtaqueCriatura(5);
+    }
+
+
+    public String getDescricao() {
+        return "" + getNomeCriatura() + ": " + String.valueOf(getVidaCriatura()) + "HP / " + String.valueOf(getAtaqueCriatura()) + "ATK";
+    }
+    public String getNomeCriatura() {
+        return nome;
+    }
+    public String setNomeCriatura(String nome) {
+        this.nome = nome;
+        return nome;
+    }
+    public int getVidaCriatura() {
+        return vida;
+    }
+    public void setVidaCriatura(int vida) {
+        this.vida = vida;
+    }
+    public int getAtaqueCriatura() {
+        return ataque;
+    }
+    public void setAtaqueCriatura(int ataque) {
+        this.ataque = ataque;
+    }
 }
